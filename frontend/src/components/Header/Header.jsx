3189dfd4345b5886d560e0d119b60385c32dc381
fitzgibbonjack <63608338+fitzgibbonjack@users.noreply.github.com>
2022-02-01 21:11:09 +0000

    Header: added theme toggle

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import "./Header.scss";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header__logo">
+      <span className="header__logo">
         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="26">
           <path
             fill="#FFF"
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z"
           />
         </svg>
-      </div>
+      </span>
 
-      {/* theme toggle component */}
+      <ThemeToggle className="header__toggle" />
 
       {/* user component */}
     </header>
---
+++ frontend/src/components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import "./ThemeToggle.scss";
+
+export default function ThemeToggle({ className }) {
+  const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode"));
+
+  const enableDarkMode = () => {
+    localStorage.setItem("darkMode", "true");
+    setDarkMode(localStorage.getItem("darkMode"));
+    document.body.classList.add("dark-mode");
+  };
+
+  const disableDarkMode = () => {
+    localStorage.setItem("darkMode", "false");
+    setDarkMode(localStorage.getItem("darkMode"));
+    document.body.classList.remove("dark-mode");
+  };
+
+  const toggleDarkMode = () => {
+    if (darkMode === "true") {
+      disableDarkMode();
+    } else {
+      enableDarkMode();
+    }
+  };
+
+  useEffect(() => {
+    if (darkMode === "true") {
+      enableDarkMode();
+    }
+  });
+
+  return (
+    <button
+      className={`themeToggle ${className}`}
+      type="button"
+      onClick={toggleDarkMode}
+      data-state={darkMode}
+    >
+      <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#7E88C3" fillRule="nonzero" />
+      </svg>
+    </button>
+  );
+}
